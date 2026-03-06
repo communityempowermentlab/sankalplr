6f7a009d87211fad9fbbe5b1c9ff2e18809b7ead
Mohammad Shiblee <shiblee1983@gmail.com>
2026-03-06 07:13:39 +0530

    ci: Update Vite base path to root to prevent 404 blank screen on Apache

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/sankalplr/', // Setting base path to /sankalplr
+  base: '/', // Setting base path to root for Apache VirtualHost
 })
